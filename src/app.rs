649c1092638d9ecba5ca28bff79a5b9ff2f6007c
Antonio-M. Corbi Bellot <antonio.corbi@gmail.com>
2026-03-05 17:17:23 +0100

    Adding code

--- src/app.rs
+++ src/app.rs
@@ -3,15 +3,18 @@ type Point2D = Pos2;
 
 // -- Uses: ---------------------------------------------------------------
 use delegate::delegate;
-use egui::{emath, pos2, Color32, Frame, Pos2, Rect, Sense, Stroke, Ui, Vec2, Window};
+use egui::{
+    emath::{self, RectTransform},
+    pos2, Color32, Frame, Pos2, Rect, Sense, Stroke, Ui, Vec2, Window,
+};
 
 // -- Traits: -------------------------------------------------------------
 
 trait AppUi {
     fn create_drawing_widget(&mut self, ui: &mut Ui) -> egui::Response;
     fn create_stroke_widget(&mut self, ui: &mut Ui) -> egui::Response;
-    fn draw_point(&mut self, p: Point2D, zoom: f32, painter: &egui::Painter);
-    fn draw_lines(&mut self, lines: &Vec<Pos2>, painter: &egui::Painter);
+    fn draw_point(&mut self, p: Point2D, color: Color32, zoom: f32, painter: &egui::Painter);
+    fn draw_lines(&mut self, lines: &Vec<Pos2>, color: Color32, painter: &egui::Painter);
 }
 
 // -- Types: --------------------------------------------------------------
@@ -20,7 +23,7 @@ trait AppUi {
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct FallingSandApp {
     // Data
-    data: u8,
+    data: Vec<Vec<u8>>,
 }
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -29,22 +32,54 @@ pub struct FallingSandApp {
 pub struct FallingSandAppUi {
     pub fsapp: FallingSandApp,
     pub stroke: Stroke,
+    pub world_rect: Rect,
+    pub screen_rect: Rect,
+    pub w2s: RectTransform,
+    pub s2w: RectTransform,
 }
 
 // -- Impl: ---------------------------------------------------------------
 impl Default for FallingSandApp {
     fn default() -> Self {
-        Self { data: 0 }
+        Self {
+            data: vec![vec![0; 256]; 256],
+        }
     }
 }
 
 impl Default for FallingSandAppUi {
     fn default() -> Self {
+        let fsapp = FallingSandApp::default();
+        let world_rect = fsapp.world_rect();
+        let screen_rect = Rect::ZERO;
+        let w2s = emath::RectTransform::from_to(world_rect, screen_rect);
+        let s2w = w2s.inverse();
+
         Self {
             // Example stuff:
-            stroke: Stroke::new(2.0, Color32::LIGHT_RED.linear_multiply(1.25)),
             fsapp: Default::default(),
+            stroke: Stroke::new(2.0, Color32::LIGHT_RED.linear_multiply(1.25)),
+            world_rect,
+            screen_rect,
+            w2s,
+            s2w,
         }
+    }
+}
+
+impl FallingSandApp {
+    fn nrows(&self) -> usize {
+        self.data.len()
+    }
+
+    fn ncols(&self) -> usize {
+        self.data[0].len()
+    }
+
+    fn world_rect(&self) -> Rect {
+        let min = pos2(0.0, 0.0);
+        let size: Vec2 = Vec2::new(self.ncols() as f32, self.nrows() as f32);
+        Rect::from_min_size(min, size)
     }
 }
 
@@ -62,6 +97,31 @@ impl FallingSandAppUi {
             Default::default()
         }
     }
+
+    pub fn update_transforms(&mut self, screen_rect: Rect) {
+        // Store the canvas rect
+        self.screen_rect = screen_rect;
+
+        // Compute world2screen and screen2world
+        self.w2s = emath::RectTransform::from_to(self.world_rect, self.screen_rect);
+        self.s2w = self.w2s.inverse();
+    }
+
+    pub fn pos2_to_screen(&self, pos: Pos2) -> Pos2 {
+        self.w2s.transform_pos(pos)
+    }
+
+    pub fn pos2_to_world(&self, pos: Pos2) -> Pos2 {
+        self.s2w.transform_pos(pos)
+    }
+
+    pub fn rect_to_screen(&self, rect: Rect) -> Rect {
+        self.w2s.transform_rect(rect)
+    }
+
+    pub fn rect_to_world(&self, rect: Rect) -> Rect {
+        self.s2w.transform_rect(rect)
+    }
 }
 
 // -- Impl For: -----------------------------------------------------------
@@ -72,12 +132,8 @@ impl AppUi for FallingSandAppUi {
             Sense::CLICK,
         );
 
-        let to_screen = emath::RectTransform::from_to(
-            Rect::from_min_size(Pos2::ZERO, response.rect.size()),
-            response.rect,
-        );
-
-        let from_screen = to_screen.inverse();
+        // Compute transforms: w2s + s2w
+        self.update_transforms(response.rect);
 
         // 2. Comprobamos el click izquierdo
         if response.secondary_clicked() {
@@ -97,7 +153,11 @@ impl AppUi for FallingSandAppUi {
         if response.clicked() {
             println!("¡Click izquierdo detectado en el Painter!");
             if let Some(pos) = response.interact_pointer_pos() {
-                println!("Click en la posición: {:?}", pos);
+                println!(
+                    "Click en la posición screen:{:?} / world: {:?}",
+                    pos,
+                    self.pos2_to_world(pos)
+                );
             }
         }
 
@@ -106,9 +166,10 @@ impl AppUi for FallingSandAppUi {
             // egui::Color32::RED
             self.stroke.color
         } else {
-            egui::Color32::BLUE
+            egui::Color32::LIGHT_GRAY
         };
-        painter.circle_filled(response.rect.center(), 40.0, color);
+        //painter.circle_filled(response.rect.center(), 40.0, color);
+        self.draw_point(response.rect.center(), color, 40.0, &painter);
 
         response
     }
@@ -117,7 +178,7 @@ impl AppUi for FallingSandAppUi {
         //let mut stroke: Stroke = self.stroke;
 
         ui.horizontal(|ui| {
-            ui.label(egui::RichText::new("Grains props.").color(egui::Color32::YELLOW));
+            ui.label(egui::RichText::new("Grains props.").color(egui::Color32::GREEN));
             ui.add(&mut self.stroke);
             ui.separator();
             if ui.button("Clear Canvas").clicked() {
@@ -127,7 +188,7 @@ impl AppUi for FallingSandAppUi {
         .response
     }
 
-    fn draw_point(&mut self, p: Point2D, zoom: f32, painter: &egui::Painter) {
+    fn draw_point(&mut self, p: Point2D, color: Color32, zoom: f32, painter: &egui::Painter) {
         // También puedes obtener los límites
         // let min = painter.clip_rect().min; // Esquina superior izquierda (Pos2)
         // let max = painter.clip_rect().max; // Esquina inferior derecha (Pos2)
@@ -137,20 +198,19 @@ impl AppUi for FallingSandAppUi {
         // let radio = zoom.min(3.5);
         // let radio = ((zoom + 0.125) / 2.5).max(3.5);
         // let color = Color32::from_rgb(255, 255, 255);
-        let color = Color32::CYAN;
 
         if zoom < 1.5 {
             radio = 1.5;
         }
-        if zoom > 4.0 {
-            radio = 4.0;
-        }
+        // if zoom > 4.0 {
+        //     radio = 4.0;
+        // }
 
         painter.circle_filled(centro, radio, color);
     }
 
-    fn draw_lines(&mut self, lines: &Vec<Pos2>, painter: &egui::Painter) {
-        let stroke = Stroke::new(0.5, egui::Color32::LIGHT_YELLOW);
+    fn draw_lines(&mut self, lines: &Vec<Pos2>, color: Color32, painter: &egui::Painter) {
+        let stroke = Stroke::new(0.5, color);
         painter.line(lines.to_vec(), stroke);
     }
 }
